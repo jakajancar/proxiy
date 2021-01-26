7d51bf2e8b8de40affce4a63c2fffc20c61ccf94
Jaka Jančar <jaka@kubje.org>
2021-01-26 16:12:09 -0500

    Initial Commit

---
+++ Proxy/ProxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProxyApp.swift
+//  Proxy
+//
+//  Created by Jaka Jancar on 1/26/21.
+//
+
+import SwiftUI
+
+@main
+struct ProxyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
