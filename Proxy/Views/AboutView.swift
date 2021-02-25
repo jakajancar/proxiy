bff55c0b14e471ea3ab290dfcbe0f6b18874ecd6
Jaka Jančar <jaka@kubje.org>
2021-02-24 23:50:02 -0500

    Icons!

---
+++ Proxy/Views/AboutView.swift
@@ -0,0 +1,20 @@
+//
+//  AboutView.swift
+//  Proxy
+//
+//  Created by Jaka Jancar on 2/24/21.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AboutView_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutView()
+    }
+}
