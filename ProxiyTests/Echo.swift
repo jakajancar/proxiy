6d57d445c1701d7dff2a399e17fd8e0826282aed
Jaka Jančar <jaka@kubje.org>
2021-03-20 20:32:43 -0400

    Split createEchoConnection into createEchoConnection & createEchoListener

--- ProxiyTests/Echo.swift
+++ ProxiyTests/Echo.swift
@@ -9,8 +9,7 @@ import Foundation
 import Network
 @testable import Proxiy
 
-/// Creates a TCP/UDP connection that echoes back stuff sent to it. Uses `NWConnection.forwardAllMessages`
-func createEchoConnection(params: NWParameters, completion: @escaping (NWConnection) -> Void) {
+func createEchoListener(params: NWParameters, completion: @escaping (NWListener, NWEndpoint) -> Void) {
     let listener = try! NWListener(using: params)
     listener.newConnectionHandler = { conn in
         conn.start(queue: DispatchQueue.main)
@@ -29,23 +28,29 @@ func createEchoConnection(params: NWParameters, completion: @escaping (NWConnect
         case .ready:
             // Listener ready, create connection to it
             let listenerEndpoint = NWEndpoint.hostPort(host: .name("localhost", nil), port: listener.port!)
-            let client = NWConnection(to: listenerEndpoint, using: params)
-            client.stateUpdateHandler = { status in
-                switch status {
-                case .ready:
-                    completion(client)
-                case .failed(_):
-                    // the dual connect aborted connection also fails here
-                    break
-                default:
-                    break
-                }
-            }
-            client.start(queue: DispatchQueue.main)
-
+            completion(listener, listenerEndpoint)
         default:
             break
         }
     }
     listener.start(queue: DispatchQueue.main)
+}
+
+/// Creates a TCP/UDP connection that echoes back stuff sent to it. Uses `NWConnection.forwardAllMessages`
+func createEchoConnection(params: NWParameters, completion: @escaping (NWConnection) -> Void) {
+    createEchoListener(params: params) { (listener, listenerEndpoint) in
+        let client = NWConnection(to: listenerEndpoint, using: params)
+        client.stateUpdateHandler = { status in
+            switch status {
+            case .ready:
+                completion(client)
+            case .failed(_):
+                // the dual connect aborted connection also fails here
+                break
+            default:
+                break
+            }
+        }
+        client.start(queue: DispatchQueue.main)
+    }
 }
